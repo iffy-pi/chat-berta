a9f2ec5c6d1816a407641555490a0ce1b3d7e698
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2022-12-23 23:14:19 -0500

    Fixed merge issues

--- app.py
+++ app.py
@@ -18,18 +18,6 @@ app = Flask(__name__)
     #     return "Testing web page!"
     # we can have several routes for the different pages on our website
     # just by adding more app routes and the subsequent functions that handle them
-# App routing information
-    # now we use app routing to map a function to a given page of our website
-    # in app routing, it starts from the root of our website
-    # so if our website is mysite.com, and we wanted to route to mysite.com/hello
-    # we would pass /hello to the app route call
-    # app routing uses special @ and then the flask app oobject
-    # then we immediately define the associated function for the URL
-    # @app.route("/test")
-    # def testfunc():
-    #     return "Testing web page!"
-    # we can have several routes for the different pages on our website
-    # just by adding more app routes and the subsequent functions that handle them
 
 
 # Uploading file, sourced from steps https://flask.palletsprojects.com/en/2.2.x/patterns/fileuploads/
@@ -86,9 +74,6 @@ def route_chat_transcript():
 # for the root of the website, we would just pass in "/" for the url
 @app.route('/')
 def index():
-    # render index html which contains the form
-    # form submission will route to /submitForm
-    return render_template('index.html')
     # render index html which contains the form
     # form submission will route to /submitForm
     return render_template('index.html')
