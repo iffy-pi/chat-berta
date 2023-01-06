b2f6e5a5af3c469b4bce5de015d844ceeb77d595
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-06 10:51:24 -0500

    Moved model folder to under utils

--- model/evaluate.py
+++
@@ -1,6 +0,0 @@
-from rouge import Rouge
-def rouge_score(generated_summary, reference_summary) -> float:
-    rouge = Rouge()
-    scores = rouge.get_scores(generated_summary, reference_summary)
-    rouge_1_f = scores[0]['rouge-1']['f']
-    return rouge_1_f
