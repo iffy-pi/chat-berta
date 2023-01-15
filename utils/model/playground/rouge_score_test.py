b414793902548504dc6f141daaa1bfec68e35cb0
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-15 15:05:49 -0500

    Moved model/ folder from utils to apiutils/

--- utils/model/playground/rouge_score_test.py
+++
@@ -1,9 +0,0 @@
-from rouge_score import rouge_scorer
-
-scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
-scores = scorer.score('The quick brown fox jumps over the lazy dog',
-                      'The quick brown dog jumps on the log.')
-scores = scorer.score('The quick brown fox jumps over the lazy dog',
-                      'The quick brown fox jumps over the lazy dog')
-
-print(scores)
