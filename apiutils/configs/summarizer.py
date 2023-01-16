9c753171f6e6a93d16bb17f31ae45831c596b3c5
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-15 23:25:57 -0500

    Addition of common directory and loading the json file within it

--- apiutils/configs/summarizer.py
+++ apiutils/configs/summarizer.py
@@ -1,4 +1,29 @@
-SUMMARIZER_OPTIONS = [
-    ('UseStrict', 'Strict summary'),
-    ('TreatAsMonologue', 'Treat transcript as monologue')
-]
+import json
+import os
+
+def load_options():
+    try:
+        # read the json file
+        read_options = open(os.path.join('..', 'src', 'common', 'config.json')).read()
+
+        # convert to actual python dict
+        read_options = json.loads(read_options)
+
+        # then convert to python tuple
+        opts = []
+        for opt in read_options['summarizerOptions']:
+            opts.append((opt['tag'], opt['desc']))
+
+        return opts
+
+    except Exception as e:
+        return [
+            ('Failed', 'Failed: {}'.format(e))
+        ]
+
+SUMMARIZER_OPTIONS = load_options()
+
+# SUMMARIZER_OPTIONS = [
+#     ('UseStrict', 'Strict summary'),
+#     ('TreatAsMonologue', 'Treat transcript as monologue')
+# ]
