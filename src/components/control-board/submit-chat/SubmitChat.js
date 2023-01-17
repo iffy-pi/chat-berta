33a8fdd3135bd13558bac902a2172266407bda9d
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-17 03:10:59 -0500

    Added input error handling

--- src/components/control-board/submit-chat/SubmitChat.js
+++ src/components/control-board/submit-chat/SubmitChat.js
@@ -1,10 +1,10 @@
 import UploadChatFile from "./UploadChatFile";
 import UploadChatText from "./UploadChatText";
-import FileUploader from '../../common/FileUploader'
 import { useState, useRef } from "react"
 import Button from "../../common/Button";
 import SummarizerOptions from "./SummarizerOptions";
 import data from "../../../shared/config.json"
+import { goodChatFileUpload, readFileToText } from "../../../functions/basefunctions";
 
 const inputOptions = {
     def: 0,
@@ -33,6 +33,8 @@ const SubmitChat = () => {
 
     const transcriptText = useRef('')
     const summaryOptions = useRef(defaultSummarizerOptions)
+    const selectedFile = useRef(null)
+    const fileUploaded = useRef(false)
 
     const saveTranscriptText = (textboxText) => {
        transcriptText.current = textboxText 
@@ -42,17 +44,57 @@ const SubmitChat = () => {
         summaryOptions.current = options
     }
 
+    const failedFileUpload = ( error ) => {
+        alert('File upload failed!\nError: '+error)
+    }
+
+    const goodFileUpload = ( file ) => {
+        selectedFile.current = file
+        fileUploaded.current = true
+    }
+
+    const onSubmit = async () => {
+
+        let uploadContent = ""
+
+        try {
+            if ( selectedInput === inputOptions.def ) throw new Error('No chat input selected!')
+
+            if ( selectedInput === inputOptions.file ) {
+                
+                if ( !fileUploaded.current ) throw new Error('No file or invalid file uploaded!')
+                if ( !goodChatFileUpload(selectedFile.current) ) throw new Error('Invalid file type. Only text based files are allowed.')
+
+                // read the contents from the file
+                uploadContent = await readFileToText(selectedFile.current)
+            }
+
+            else if ( selectedInput === inputOptions.transcript ) {
+                if ( transcriptText.current === "" ) throw new Error('No transcript text!')
+                uploadContent = transcriptText
+            }
+
+        } catch(error){
+            alert('Submission Failed!\nReason: '+error)
+            return
+        }
+
+        console.log('Valid submission')
+        console.log(summaryOptions.current)
+        console.log(uploadContent)
+
+    }
+
     return (
         <div className="basic-container">
             <h1>{data.test}</h1>
             <Button buttonText="Transcript" onClick={() => setSelectedInput(inputOptions.transcript)}/>
             <Button buttonText="Upload File" onClick={() => setSelectedInput(inputOptions.file)}/>
-            { (selectedInput === inputOptions.file) && <UploadChatFile />}
+            { (selectedInput === inputOptions.file) && <UploadChatFile goodFileUpload={goodFileUpload} failedFileUpload={failedFileUpload}/>}
             { (selectedInput === inputOptions.transcript) && <UploadChatText returnText={saveTranscriptText}/>}
             { (selectedInput === inputOptions.def) && <br />}
             <SummarizerOptions options={summaryOptions.current} returnOptions={updateSelectedOptions}/>
-            <Button buttonText="Summarize!" onClick={() => alert('Submitted!: '+ summaryOptions.current[0] +' '+summaryOptions.current[0].selected)}/>
-            {/* <FileUploader /> */}
+            <Button buttonText="Summarize!" onClick={onSubmit}/>
 
         </div>
     )
