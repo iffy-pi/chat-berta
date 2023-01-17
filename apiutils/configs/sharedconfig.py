9006e02d96bbb8597b98dde3031fded4323456f4
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-17 00:53:04 -0500

    Implementing shared config file for api better

---
+++ apiutils/configs/sharedconfig.py
@@ -0,0 +1,15 @@
+# Contains the shared config in a python file
+# Done so that the file is read once and cached for other scripts to use
+import os
+import json
+
+def load_shared_config():
+    try:
+        # read the json file and convert to dictionary
+        shared_config = json.loads( open( os.path.join('..', 'src', 'shared', 'config.json') ).read() )
+        return shared_config
+
+    except Exception as e:
+        return None
+
+SHARED_CONFIG = load_shared_config()
