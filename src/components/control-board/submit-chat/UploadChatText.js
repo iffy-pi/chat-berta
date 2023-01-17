07ca2ee501edac5fbe65e8796c4abcd01ee9f32a
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-01-17 00:36:24 -0500

    Updating SubmitChat Text to reduce number of re-renders

--- src/components/control-board/submit-chat/UploadChatText.js
+++ src/components/control-board/submit-chat/UploadChatText.js
@@ -1,8 +1,25 @@
-const UploadChatText = ({ transcriptText, setTranscriptText }) => {
+import { useState } from "react"
+
+const UploadChatText = ({ returnText }) => {
+    const [ _transcriptText, _setTranscriptText ] = useState('')
+
+    const onTextChange = (newText) => {
+        // we set the transcript text and return to parent component
+        returnText(newText)
+        // Also update our current state
+        _setTranscriptText(newText)
+    }
+
     return (
         <div className="basic-container">
             <p><label htmlFor="transcript_text_box">Paste Dialog Transcript Below:</label></p>
-            <textarea id="transcript_text_box" name="transcript_text" rows="10" cols="75" placeholder="Type/Paste here!" value={transcriptText} onChange={(e) => setTranscriptText(e.target.value)}></textarea>
+            <textarea 
+            id="transcript_text_box" 
+            name="transcript_text" 
+            rows="10" cols="75" 
+            placeholder="Type/Paste transcript here!" 
+            value={_transcriptText} 
+            onChange={(e) => onTextChange(e.target.value)} />
         </div>
     )
 }
