1e6cb6a2a646cd12fa5b924627e2fc4193975a09
Hai-Ling R <50031585+hai-ling@users.noreply.github.com>
2023-01-23 16:30:11 -0500

    incomplete push 2
    pick_one_sentence_demo working, some tokenization problems with training still work although the data pipeline seems to be working. compute_loss needs to return proper loss

--- apiutils/model/playground/tokenize_test.py
+++ apiutils/model/playground/tokenize_test.py
@@ -1,14 +1,14 @@
 from transformers import RobertaTokenizer
-from datasets import load_dataset
-dataset = load_dataset("samsum", split="train")
-# Load the tokenizer
+
+# Instantiate the tokenizer
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
 # Tokenize the input text
-input_text = dataset[0]['dialogue']#.split("\r\n")
-#input_text = [line + "\r\n" for line in input_text]
-print(input_text)
-tokens = tokenizer(input_text)
-converted_tokens = tokenizer.convert_ids_to_tokens([0, 10127, 5219, 35, 38, 17241, 1437, 15269, 4, 1832, 47, 236, 103, 116, 2])
+text = "This is a sample paragraph of text that we want to tokenize for fine-tuning a RoBERTa model."
+tokenized_text = tokenizer.tokenize(text)
+print(tokenized_text)
 
-print(converted_tokens)
+# Convert the tokens to their corresponding IDs
+input_ids = tokenizer.convert_tokens_to_ids(tokenized_text)
+print(input_ids)
+
