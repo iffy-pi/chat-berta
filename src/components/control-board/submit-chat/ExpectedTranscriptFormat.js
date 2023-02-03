2d214b41a637469f7a6ba6d0d4f6beab4dfad005
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-02-02 22:37:04 -0500

    Updates to fix issue with Expected Transcript Format code block running out of bounds

--- src/components/control-board/submit-chat/ExpectedTranscriptFormat.js
+++ src/components/control-board/submit-chat/ExpectedTranscriptFormat.js
@@ -4,32 +4,26 @@ import Button from "../../common/Button"
 // JS component that just contains the expected format for the transcript
 const ExpectedTranscriptFormat = () => {
 
-    const [ chatExample, setChatExample ] = useState("")
+    const [ chatExample, setChatExample ] = useState("John:\nApples are my favourite fruit, what are yours?\n\nJane:\nI like oranges better, apples be gross sometime\n\nJohn:\nOh really, I think apples are superior\n\nJane:\nInteresting")
     const [ showExample, setShowExample ] = useState(false)
 
     const toggleShowExample = () => {
         setShowExample(!showExample)
     }
 
+    
+
     return (
         <div className='basic-container'>
-            <Button buttonText={ (showExample) ? "Hide Transcript Format" : "Show Transcript Format" } onClick={toggleShowExample}/>
+            <h2>Expected Chat Format</h2>
             {
                 ( showExample ) && 
                 <div>
-                    <h2>Expected Chat Format</h2>
-                    <p>Chats include the party names indicated with a colon, and the messages sent by each party. Party messages are indented (either by spaces or tabs) from the beginning of the line to differentiate them from the party sender lines. An example is included below:</p>
-                    <pre><code>John:
-            I like apples
-            What is your favourite fruit?
-        Jane:
-            I like oranges better, apples be gross sometimes
-        John:
-            Oh really, I think apples are superior
-        Janet:
-            Interesting</code></pre>
+                    <p>Chats include the party names indicated with a colon, and the messages sent by each party. Party messages are indented (either by spaces or tabs) from the beginning of the line to differentiate them from the party sender lines. An example is shown below:</p>
+                    <code className="code-block">{chatExample}</code>
                 </div>
             }
+            <Button buttonText={ (showExample) ? "Hide Transcript Format" : "Show Transcript Format" } onClick={toggleShowExample}/>
         </div>
     )
 }
