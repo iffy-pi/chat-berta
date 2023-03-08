851302f8d432796b07567669b0f7973e5f1a1dff
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-03-08 16:02:13 -0500

    Fixed Network Component to match frontend integration

--- apiutils/nec/NetworkComponent.py
+++ apiutils/nec/NetworkComponent.py
@@ -1,32 +1,31 @@
-import json
 # network component with the expected classes
 # contains
 class NetworkComponent:
     def __init__(self):
-        self.parties=[]
-        self.messages=[]
-    def json_packet_to_summarizer(self, json_packet):
-        
-        # Load JSON data to be stored
-        data = json.loads(json_packet)
-        # Store packet values
-        for party in data['config']['parties']:
-            self.parties.append({'id': party['id'], 'name': party['name']})
-        for message in data['messages']:
-            self.messages.append({'id': message['id'], 'pid': message['pid'], 'text': message['text']})
+        self.chat_package = {}
+        self.summarizer_options = {}
 
-    def generate_summary(self, json_packet):
-        self.json_packet_to_summarizer(json_packet)
-        # TODO: link output ids to summarizer NN output
-        self.output_ids = self.messages[0]['id']
-        self.create_json_packet()
+    # loads the nc with the chat package and summarizer options
+    def load(self, chat_package, summarizer_options):
+        self.chat_package = chat_package
+        self.summarizer_options = summarizer_options
 
-    def create_json_packet(self):
-        # Create a packet containing the input/output string pair
-        packet = {
-            'config': {'parties': self.parties},
-            'messages': self.messages,
-            'output_messages': list(filter(lambda x: x['id'] == self.output_ids, self.messages))
-        }
+    # runs the summarization and returns the chat_package, and any other metrics
+    def summarize(self):
+        # check summmarize_only_for_field and filter out messages that arent the party id
 
-        self.json = json.dumps(packet)
+        # convert message JSON objects into whatever format the model needs
+        # HAILING TODO: How does the model receive input? You will need to track an id for every sentence the model takes in
+
+        # convert model ouput into the summary messages format, will need the model output to have 
+        # the message ids so that we can map the party info
+        # HAILING TODO: Give model outputs and we need the message ids
+
+        # complete the chat package by adding summary chat messages field
+
+        # return along with metrics
+        pass
+
+
+
+    
