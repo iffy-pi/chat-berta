0949d8f5618a0943192adff606aae0e2a470e1c9
iffy-pi <59808927+iffy-pi@users.noreply.github.com>
2023-03-08 22:54:04 -0500

    Some clean up of the ChatBerta file

--- apiutils/model/ChatBerta.py
+++ apiutils/model/ChatBerta.py
@@ -3,6 +3,7 @@ from chat_bert_trainer import CustomTrainer
 import torch
 from constants import BATCH_SIZE, MAX_LENGTH
 PATH = r"C:\Users\rao_h\Documents\GitHub\Chat-Berta\apiutils\model\temp\checkpoint-61000"
+
 class ChatBerta:
     def __init__(self):
         ## only for initial, replace with actual directory path
@@ -68,80 +69,3 @@ class ChatBerta:
         summary  = self._reconstruct(chosen_message_ids, messages)
 
         return chosen_message_ids, summary
-
-
-if __name__ == "__main__":
-    messages = {
-"messages": [
-{
-"id": 0,
-"pid": 0,
-"text": "Apples are my favourite fruit, what are yours?"
-},
-{
-"id": 1,
-"pid": 1,
-"text": "I like oranges better, apples be gross sometimes"
-},
-{
-"id": 2,
-"pid": 0,
-"text": "Oh really? I think apples are superior."
-},
-{
-"id": 3,
-"pid": 1,
-"text": "Interesting."
-},
-{
-"id": 4,
-"pid": 0,
-"text": "Why do you think oranges are better than apples?"
-},
-{
-"id": 5,
-"pid": 1,
-"text": "I think oranges are sweeter and juicier than apples."
-},
-{
-"id": 6,
-"pid": 0,
-"text": "But apples have a wider variety of flavors and textures."
-},
-{
-"id": 7,
-"pid": 1,
-"text": "I guess that's true, but I just prefer oranges."
-},
-{
-"id": 8,
-"pid": 0,
-"text": "Have you ever tried a Honeycrisp apple? They're amazing."
-},
-{
-"id": 9,
-"pid": 1,
-"text": "No, I haven't. I'll have to try one sometime."
-},
-{
-"id": 10,
-"pid": 0,
-"text": "Definitely do. They're the perfect combination of sweet and tart."
-},
-{
-"id": 11,
-"pid": 1,
-"text": "I'll keep that in mind. Thanks for the recommendation."
-}
-]
-}
-    messages = messages["messages"]
-    summary_options = {
- "basic_options": [],
- "summarize_only_for": -1 
- }
-    chat_berta = ChatBerta()
-    chosen_message_ids, summary = chat_berta.summarize(messages=messages, summary_options = summary_options)
-
-    print(summary)
-    print(chosen_message_ids)
